5105a7075a3b03a72b08bb8a13a1e1da3808044b
Prajwal Nimbalkar <pnimbalkar@ptc.com>
2025-10-19 12:01:23 +0530

    Add advanced math operations: power, factorial, squareRoot, isPrime

--- calculator.h
+++ calculator.h
@@ -7,6 +7,12 @@ public:
     int subtract(int a, int b);
     int multiply(int a, int b);
     double divide(int a, int b);
+    
+    // Advanced math operations
+    int power(int base, int exponent);
+    int factorial(int n);
+    double squareRoot(double x);
+    bool isPrime(int n);
 };
 
 #endif // CALCULATOR_H
